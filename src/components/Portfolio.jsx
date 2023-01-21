39bb5717dc135601234eec8359ec83b9ce15ea1d
Dario Balmaceda <dario26081993@uotlook.com.ar>
2023-01-21 15:04:54 -0300

    portfolio listo

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,19 +10,23 @@ const Portfolio = () =>{
     const proyects = [
         {
             id: 1,
-            src: proyect1
+            src: proyect1,
+            href: "https://github.com/dbandres/Proyecto_Individual"
         },
         {
             id: 2,
-            src: proyect2
+            src: proyect2,
+            href: "https://github.com/dbandres/pern"
         },
         {
             id: 3,
-            src: proyect3
+            src: proyect3,
+            href: "https://github.com/dbandres/FEmentor-notification-page"
         },
         {
             id: 4,
-            src: proyect4
+            src: proyect4,
+            href: "https://github.com/dbandres/FEmentor-rating-component"
         },
     ]
 
@@ -35,16 +39,16 @@ const Portfolio = () =>{
                 </div>
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 md:pb-12 sm:mt-10">
                     {
-                        proyects.map(({id, src})=>{
+                        proyects.map(({id, src, href})=>{
                             return(
                                 <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
                                         <img src={src} alt="" className="rounded-md duration-200 hover:scale-105 w-80 h-72 md:m-auto" />
                                     <div className="flex items-center justify-center">
                                         <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                                            Demo
+                                            <a href={href} target="_blank">Demo</a>
                                         </button>
                                         <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                                            Code
+                                        <a href={href} target="_blank">Code</a>
                                         </button>
                                     </div>
                                 </div> 
